9c5a1796a2c3437dff8a310400027c2bf1442b02
Damian <37978110+dszyszek@users.noreply.github.com>
2018-09-02 04:01:50 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose.js');
 let {toDo} = require('./models/toDo.js');
@@ -48,6 +49,25 @@ app.get('/todos', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+        console.log('ID is invalid!');
+    }
+    toDo.findById(id).then((docs) => {
+        if (!docs) {
+            console.log('ID not found');
+            return res.status(404).send();
+        }
+
+        res.send({docs});
+        console.log('To-do', docs);
+    }).catch((err) => res.status(400).send());
 
 });
 
