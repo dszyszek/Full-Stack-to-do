93442e8cc34913423630eb811f3b3051bda1ed61
Damian <37978110+dszyszek@users.noreply.github.com>
2018-09-13 20:31:19 +0200

    Add authentication method

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,20 @@
+const {user} = require('../models/user.js');
+
+let authenticate = (req, res, next) => {
+
+    let token = req.header('x-auth');
+
+    user.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        req.user = user;
+        req.token = token;
+        next();
+    }).catch(e => res.status(401).send());
+};
+
+module.exports = {
+    authenticate
+};
